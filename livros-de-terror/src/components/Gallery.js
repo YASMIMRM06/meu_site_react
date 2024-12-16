c60e8d8d01494a4a2f729da914a4b2c4187451f8
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-16 14:29:07 -0300

    Update Gallery.js

--- livros-de-terror/src/components/Gallery.js
+++ livros-de-terror/src/components/Gallery.js
@@ -5,9 +5,9 @@ const Gallery = () => {
         <div>
             <h2>Galeria de Livros de Terror</h2>
             <div className="gallery">
-                <img src={`${process.env.PUBLIC_URL}/img/livro1.jpg`} alt="Livro 1" />
-                <img src={`${process.env.PUBLIC_URL}/img/livro2.jpg`} alt="Livro 2" />
-                <img src={`${process.env.PUBLIC_URL}/img/livro3.jpg`} alt="Livro 3" />
+                <img src={`${process.env.PUBLIC_URL}/img/Dracula.jpg`} alt="Livro 1" />
+                <img src={`${process.env.PUBLIC_URL}/img/baba yaga.jpg`} alt="Livro 2" />
+                <img src={`${process.env.PUBLIC_URL}/img/outsider.jpg`} alt="Livro 3" />
             </div>
         </div>
     );
