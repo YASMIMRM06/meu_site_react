7aed5ce512a16a3b23f725c83df89f434e4801b5
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-17 17:10:25 -0300

    Update Footer.js

--- myreactapp/src/components/Footer.js
+++ myreactapp/src/components/Footer.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
 import { Link } from 'react-router-dom';
-function Footer (){
-    return(
-        <footer>
+function Footer() {
+  return (
+    <footer>
       &copy; Tagline Backery Cakes
-      <br /> Rua 123, 12345-678, Cidade, Estado, CEP, Brasil.
-      <br /> 2034 Yasmim Russi Mariano - Todos os direitos reservados.
-      <div></div>
-      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-        <img id="social" src="img/twitter.png" alt="Twitter" className="social" />
-      </a>
-
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-        <img id="social" src="img/instagram.png" alt="Instagram" className="social" />
-      </a>
-
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-        <img id="social" src="img/facebook.png" alt="Facebook" className="social" />
-      </a>
+      <br />
+      Rua 123, 12345-678, Cidade, Estado, CEP, Brasil.
+      <div className="redes-sociais">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src="img/twitter.png" alt="Twitter" className="social" />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/instagram.png" alt="Instagram" className="social" />
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="img/facebook.png" alt="Facebook" className="social" />
+        </a>
+      </div>
     </footer>
-    );
+  );
 }
+
 export default Footer;
