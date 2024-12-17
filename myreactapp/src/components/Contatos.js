5f09c5d56ff06229875500a6fcef15fd9d774e88
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-17 17:09:27 -0300

    Update Contatos.js

--- myreactapp/src/components/Contatos.js
+++ myreactapp/src/components/Contatos.js
@@ -1,22 +1,31 @@
-import React from 'react';
-function contatos (){
-    return(
-        <main>
-      <h1>Contato</h1>
-      <form action="envio.php" method="POST">
-        <label htmlFor="name">Nome:</label>
-        <input type="text" id="name" name="name" required />
+import React from "react";
 
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-
-        <label htmlFor="message">Mensagem:</label>
-        <textarea id="message" name="message" rows="5" required></textarea>
-
-        <button type="submit">Enviar</button>
+function Contato() {
+  return (
+    <section id="contato">
+      <h1>Entre em Contato</h1>
+      <form>
+        <div className="grid-container">
+          <div className="grid-item">
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required />
+          </div>
+          <div className="grid-item">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="grid-item">
+            <label htmlFor="mensagem">Mensagem:</label>
+            <textarea id="mensagem" name="mensagem" rows="5" required></textarea>
+          </div>
+          <div className="grid-item">
+            <button type="submit">Enviar</button>
+          </div>
+        </div>
       </form>
-    </main>
-    );
-
+    </section>
+  );
 }
-export default Contatos;
+
+export default Contato;
+
