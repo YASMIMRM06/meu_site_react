493653c6d42bd5645b214c80bc9371e046e68640
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-17 10:59:33 -0300

    Update Footer.js

--- myreactapp/src/components/Footer.js
+++ myreactapp/src/components/Footer.js
@@ -3,20 +3,21 @@ import { Link } from 'react-router-dom';
 function Footer (){
     return(
         <footer>
-        &copy; Book Haven<br>Brasil.
-        <br>2024 Yasmim Russi Mariano - Todos os direitos reservados.
-        <div></div>
-        <a href="https://twitter.com" target="_blank">
-            <img src="https://github.com/YASMIMRM06/WEB/blob/main/SITE%20COM%20GRID/img/twitter.png?raw=true" alt="Twitter" class="social">
-        </a>
+      &copy; Tagline Backery Cakes
+      <br /> Rua 123, 12345-678, Cidade, Estado, CEP, Brasil.
+      <br /> 2034 Yasmim Russi Mariano - Todos os direitos reservados.
+      <div></div>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <img id="social" src="img/twitter.png" alt="Twitter" className="social" />
+      </a>
 
-        <a href="https://instagram.com" target="_blank">
-            <img src="https://github.com/YASMIMRM06/WEB/blob/main/SITE%20COM%20GRID/img/instagram.png?raw=true" alt="Instagram" class="social">
-        </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <img id="social" src="img/instagram.png" alt="Instagram" className="social" />
+      </a>
 
-        <a href="https://facebook.com" target="_blank">
-            <img src="https://github.com/YASMIMRM06/WEB/blob/main/SITE%20COM%20GRID/img/facebook.png?raw=true" alt="Facebook" class="social">
-        </a>
+      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <img id="social" src="img/facebook.png" alt="Facebook" className="social" />
+      </a>
     </footer>
     );
 }
