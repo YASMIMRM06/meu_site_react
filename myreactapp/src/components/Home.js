1e02aa3cf495327edadd98ee5bdf5aa704b61a4a
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-17 11:00:37 -0300

    Update Home.js

--- myreactapp/src/components/Home.js
+++ myreactapp/src/components/Home.js
@@ -1,18 +1,18 @@
 import React from 'react';
 function Home (){
     return(
-        <main>
-    <section id="home">
+       
+    <main>
+      <section id="home">
         <h1>Olá, nós somos</h1>
-        <h2>Book Haven</h2>
+        <h2>A Tagline Backery Shop</h2>
         <h2>Bem-Vindo ao nosso site!</h2>
-    </section>
-
-    <section>
-        <img id="home-img" src="https://github.com/YASMIMRM06/WEB/blob/main/Site%20com%20menu%20Sanduiche/img/leitor%20e%20trabalho.jpg?raw=true" alt="Imagem de uma biblioteca aconchegante">
-    </section>
-</main>
-    );
-
+      </section>
+      <section>
+        <img id="chocolate" src="img/front-view-delicious-chocolate-cake-concept.jpg" alt="delicious-chocolate-cake" />
+      </section>
+    </main>
+  );
 }
+
 export default Home;
